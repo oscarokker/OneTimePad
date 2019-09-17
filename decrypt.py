3cb6bbc0032f8673cfca4d59efba1511c1ca131f
oscarokker <31986880+oscarokker@users.noreply.github.com>
2019-09-17 13:20:46 +0200

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,8 @@
+import pickle
+
+import noname
+
+class DecryptFrame(noname.DecryptFrame):
+    def __init__(self, parent):
+
+        noname.DecryptFrame.__init__(self, parent)
---
+++ encrypt.py
@@ -0,0 +1,9 @@
+import pickle
+
+import noname
+
+class EncryptFrame(noname.EncryptFrame):
+    def __init__(self, parent):
+
+        noname.EncryptFrame.__init__(self, parent)
+
